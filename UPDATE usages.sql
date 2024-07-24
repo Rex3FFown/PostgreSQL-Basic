f5d0da11d20aab3f26ef0db435e3ffb73571cca0
Mahmut Altunkaya <55110329+Rex3FFown@users.noreply.github.com>
2024-07-24 22:14:47 +0300

    PostgreSQL Codes

---
+++ UPDATE usages.sql
@@ -0,0 +1,7 @@
+UPDATE ma_ucak_durum SET son_bakim_tarihi = '2024-05-29' WHERE id= 1;
+UPDATE ma_ucak_durum SET son_bakim_tarihi = '2024-06-15' WHERE id = 2;
+UPDATE ma_ucak_durum SET son_bakim_tarihi = '2024-07-10' WHERE id = 3;
+UPDATE ma_ucak_durum SET son_bakim_tarihi = '2024-05-20' WHERE id = 4;
+UPDATE ma_ucak_durum SET son_bakim_tarihi = '2024-07-05' WHERE id = 5;
+UPDATE ma_ucak_durum SET son_bakim_tarihi = '2024-06-25' WHERE id = 6;
+UPDATE ma_ucak_durum SET son_bakim_tarihi = '2024-07-12' WHERE id = 7;
