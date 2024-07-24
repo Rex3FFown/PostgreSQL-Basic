cfcb0fe5d09d8739e6f433c255393068cf4fe685
Mahmut Altunkaya <55110329+Rex3FFown@users.noreply.github.com>
2024-07-24 22:06:47 +0300

    Create INSERT INTO usage.sql

---
+++ INSERT INTO usage.sql
@@ -0,0 +1,13 @@
+INSERT INTO ma_ucak_durum (id, ucak_model, yakit_orani, toplam_km)
+VALUES
+(1, 'Boeing 737', 35, 12000),
+(2, 'Airbus A320', 42, 9000),
+(3, 'Boeing 747', 50, 15000),
+(4, 'Airbus A380', 55, 13000),
+(5, 'Embraer 190', 30, 7000),
+(6, 'Cessna 172', 25, 2000),
+(7, 'Gulfstream G650', 40, 8000),
+(8, 'Bombardier CRJ700', 38, 6000),
+(9, 'Boeing 787', 45, 10000),
+(10, 'Airbus A350', 50, 11000),
+(11, 'Airbus A350', 50, 11000);
