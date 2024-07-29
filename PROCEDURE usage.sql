1467cd79d578f016f1189d05d03afc5b6222eb29
Mahmut Altunkaya <55110329+Rex3FFown@users.noreply.github.com>
2024-07-29 14:42:16 +0300

    PostgreSQL Codes

--- PROCEDURE usage.sql
+++ PROCEDURE usage.sql
@@ -9,3 +9,15 @@ $$;
 --After call for procedure
 
 CALL ma_deneme();
+
+
+--------------------------------------------
+CREATE PROCEDURE ma_parametre(a1 INT, a2 INT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RAISE NOTICE '%', a1 + a2;
+END
+$$;
+
+CALL ma_parametre(1, 5);
