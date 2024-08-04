e0f2370872304b253aa12a302a2f3a8ab3e93c4d
Mahmut Altunkaya <55110329+Rex3FFown@users.noreply.github.com>
2024-08-04 17:56:21 +0300

    Chess with PostgreSQL

--- Chess with PostgreSQL/Function of Forming the Chessboard.sql
+++ Chess with PostgreSQL/Function of Forming the Chessboard.sql
@@ -2,18 +2,18 @@ CREATE OR REPLACE FUNCTION tahta_oluştur(oyun_id INT) RETURNS VOID AS $$
 BEGIN
     -- Beyaz taşlar
     INSERT INTO tahtalar (oyun_id, konum, taş) VALUES 
-    (oyun_id, 'a1', 'br'), (oyun_id, 'b1', 'bn'), (oyun_id, 'c1', 'bb'),
-    (oyun_id, 'd1', 'bv'), (oyun_id, 'e1', 'bş'), (oyun_id, 'f1', 'bb'),
-    (oyun_id, 'g1', 'bn'), (oyun_id, 'h1', 'br');
+    (oyun_id, 'a1', 'bk'), (oyun_id, 'b1', 'ba'), (oyun_id, 'c1', 'bf'),
+    (oyun_id, 'd1', 'bv'), (oyun_id, 'e1', 'bş'), (oyun_id, 'f1', 'bf'),
+    (oyun_id, 'g1', 'ba'), (oyun_id, 'h1', 'bk');
 
     INSERT INTO tahtalar (oyun_id, konum, taş)
     SELECT oyun_id, CHR(96 + i) || '2', 'bp' FROM generate_series(1, 8) AS s(i);
 
     -- Siyah taşlar
     INSERT INTO tahtalar (oyun_id, konum, taş) VALUES 
-    (oyun_id, 'a8', 'sr'), (oyun_id, 'b8', 'sn'), (oyun_id, 'c8', 'sb'),
-    (oyun_id, 'd8', 'sv'), (oyun_id, 'e8', 'sş'), (oyun_id, 'f8', 'sb'),
-    (oyun_id, 'g8', 'sn'), (oyun_id, 'h8', 'sr');
+    (oyun_id, 'a8', 'sk'), (oyun_id, 'b8', 'sa'), (oyun_id, 'c8', 'sf'),
+    (oyun_id, 'd8', 'sv'), (oyun_id, 'e8', 'sş'), (oyun_id, 'f8', 'sf'),
+    (oyun_id, 'g8', 'sa'), (oyun_id, 'h8', 'sk');
 
     INSERT INTO tahtalar (oyun_id, konum, taş)
     SELECT oyun_id, CHR(96 + i) || '7', 'sp' FROM generate_series(1, 8) AS s(i);
